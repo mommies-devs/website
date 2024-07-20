9f154998c3f9e134497390144ec294821a1b8c25
maxim <bozzhik@ya.ru>
2024-07-20 04:28:01 +0300

    ~ refactor `Container` Global component

--- src/components/Global/Container.tsx
+++ src/components/Global/Container.tsx
@@ -5,14 +5,14 @@ interface Props {
   className: string
 }
 
+const containerStyles = {
+  width: 'w-[660px] md:w-[600px] sm:w-auto',
+  padding: 'pt-[250px] xl:pt-[200px] md:pt-[150px] sm:pt-[125px]',
+  margin: 'mx-auto sm:mx-4',
+}
+
+const {width, padding, margin} = containerStyles
+
 export default function Container({children, className}: Props) {
-  const containerStyles = {
-    width: 'w-[660px] md:w-[600px] sm:w-auto',
-    padding: 'pt-[250px] xl:pt-[200px] md:pt-[150px] sm:pt-[125px]',
-    margin: 'mx-auto sm:mx-4',
-  }
-
-  const {width, padding, margin} = containerStyles
-
   return <main className={cn([width, padding, margin], className)}>{children}</main>
 }
