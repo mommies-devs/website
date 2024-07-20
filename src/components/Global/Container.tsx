4fa28cf21626f292717d85fc5b8e700aef21a45c
maxim <bozzhik@ya.ru>
2024-07-20 04:42:51 +0300

    add `padding bottom` for `Container` Global component

--- src/components/Global/Container.tsx
+++ src/components/Global/Container.tsx
@@ -7,12 +7,13 @@ interface Props {
 
 const containerStyles = {
   width: 'w-[660px] md:w-[600px] sm:w-auto',
-  padding: 'pt-[250px] xl:pt-[200px] md:pt-[150px] sm:pt-[125px]',
+  paddingtop: 'pt-[250px] xl:pt-[200px] md:pt-[150px] sm:pt-[125px]',
+  paddingbottom: 'pb-[175px] xl:pb-[125px] md:pb-[100px] sm:pb-[75px]',
   margin: 'mx-auto sm:mx-4',
 }
 
-const {width, padding, margin} = containerStyles
+const {width, paddingtop, paddingbottom, margin} = containerStyles
 
 export default function Container({children, className}: Props) {
-  return <main className={cn([width, padding, margin], className)}>{children}</main>
+  return <main className={cn([width, paddingtop, paddingbottom, margin], className)}>{children}</main>
 }
